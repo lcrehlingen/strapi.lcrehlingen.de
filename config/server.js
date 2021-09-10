6993e8a3f1439ce2e3ef90ee8c4aab4313eac890
Niklas Marion <niklas.marion@gmx.de>
2021-09-10 20:36:37 +0000

    test

--- config/server.js
+++ config/server.js
@@ -1,7 +1,12 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  cron :{
+    enabled: true
+  },
+  url: 'https://content.nimarion.tech',
   admin: {
+    url: 'https://test.lcrehlingen.de/cms',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '74e98953e4680255e0d8a85e410f9a33'),
     },
