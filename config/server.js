e3b280b92503962a82f57cb5f5e5decae4b1938d
Niklas Marion <niklas.marion@gmx.de>
2021-09-25 18:40:37 +0000

    remove admin url

--- config/server.js
+++ config/server.js
@@ -4,9 +4,7 @@ module.exports = ({ env }) => ({
   cron :{
     enabled: true
   },
-  url: 'https://content.nimarion.tech',
   admin: {
-    url: 'https://test.lcrehlingen.de/cms',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '74e98953e4680255e0d8a85e410f9a33'),
     },
