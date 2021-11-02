aca8201e98b632808a4430077a879f05223518d3
Niklas Marion <niklas.marion@gmx.de>
2021-11-02 14:40:09 +0000

    add admin url

--- config/server.js
+++ config/server.js
@@ -4,7 +4,10 @@ module.exports = ({ env }) => ({
   cron :{
     enabled: true
   },
+  proxy: env.bool('IS_PROXIED', true),
+  url: env('PUBLIC_URL', 'https://test.lcrehlingen.de/api/strapi'),
   admin: {
+    url: env('PUBLIC_ADMIN_URL', 'https://test.lcrehlingen.de/admin'),
     auth: {
       secret: env('ADMIN_JWT_SECRET', '74e98953e4680255e0d8a85e410f9a33'),
     },
