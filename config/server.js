15e46013d261aa257f6ad3de1ec16a4a375eb92c
Niklas Marion <niklas.marion@gmx.de>
2024-01-17 11:56:27 +0100

    change prodution url

--- config/server.js
+++ config/server.js
@@ -5,9 +5,9 @@ module.exports = ({ env }) => ({
     enabled: true
   },
   proxy: env.bool('IS_PROXIED', true),
-  url: env('PRODUCTION_URL', 'http://localhost:1337') + "/api/strapi",
+  url: env('PRODUCTION_URL', 'http://localhost:1337'),
   admin: {
-    url: env('PRODUCTION_URL', 'http://localhost:1337') + "/admin",
+    url: env('PRODUCTION_URL', 'http://localhost:1337') + '/admin',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '74e98953e4680255e0d8a85e410f9a33'),
     },
